824963150bd3f9bee9d35487de1d8c6fe75ac219
Pierce Lauring <pclauring@gmail.com>
2019-06-14 00:03:09 -0400

    Updated icon

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { rhythm } from "../utils/typography"
+import Image from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,7 +10,8 @@ class AboutPage extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
-    const linkedin = data.site.siteMetadata.social.linkedin
+    const author = data.site.siteMetadata.author
+    const { linkedin } = data.site.siteMetadata.social
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -17,6 +20,19 @@ class AboutPage extends React.Component {
           keywords={[`bio`, `c#`, `javascript`, `react`, `redux`, `gatsby`]}
         />
         <h3>About Pierce</h3>
+        <Image
+          fixed={data.avatar.childImageSharp.fixed}
+          alt={author}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            minWidth: 50,
+            borderRadius: `100%`,
+          }}
+          imgStyle={{
+            borderRadius: `50%`,
+          }}
+        />
         You can find my professional history{" "}
         <a href={`https://linkedin.com/in/${linkedin}`}>here</a>.
         <p />
@@ -29,26 +45,19 @@ export default AboutPage
 
 export const pageQuery = graphql`
   query {
-    site {
-      siteMetadata {
-        title
-        social {
-          linkedin
+    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      childImageSharp {
+        fixed(width: 50, height: 50) {
+          ...GatsbyImageSharpFixed
         }
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
+    site {
+      siteMetadata {
+        author
+        social {
+          instagram
+          linkedin
         }
       }
     }

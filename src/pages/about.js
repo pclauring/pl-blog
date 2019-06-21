177735fc0515e6bf092f3fb3699e487b36cc2712
Pierce Lauring <pclauring@gmail.com>
2019-06-21 14:31:12 -0400

    About updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -46,10 +46,10 @@ class AboutPage extends React.Component {
           />
           <p>
             I created this blog as a way to share my sketchbook progress and
-            eventually other projects I have been working on. I graduated
-            University of Michigan Ann Arbor with a BS in Biology. My interests
-            include cats, drawing, painting, rowing <strong>(erging)</strong>,
-            3D-Printing and programming.
+            eventually other projects I have been working on. I graduated from
+            the University of Michigan in Ann Arbor with a BS in Biology. My
+            interests include cats, drawing, painting, rowing{" "}
+            <strong>(erging)</strong>, 3D-Printing and programming.
           </p>
         </div>
       </Layout>
